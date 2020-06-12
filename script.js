d33948736b21fddcc5307bd1f8e523fbb73642bf
Sean de Groot <degroot.sd@gmail.com>
2020-06-11 20:04:51 -0400

    Working on F to C Button

--- script.js
+++ script.js
@@ -2,68 +2,99 @@ const app = {};
 
 app.apiUrl = `https://api.nasa.gov/insight_weather/?api_key=H0DYUa5B7S8DznB16FCH8Ym8KEmJgbwTp9eb0guB&feedtype=json&ver=1.0`
 
+// const animation = (target, string) => {
+//   $(target).fadeOut(function () {
+//     $(this).html(string).fadeIn();
+//   });
+// };
+
+// animation('.temperature-max', `${tempMax} F°`);
+// animation('.temperature-min', `${tempMin} F°`);
+// animation('.temperature-max', `${tempMax} C°`);
+// animation('.temperature-min', `${tempMin} C°`);
+
 app.getDayStats = (data) => {
   const solKeys = data.sol_keys;
 
   const weeklyTemp = [];
 
   for (let i = 0; i < solKeys.length; i++) {
-    const dayStats = {
-      solDay: solKeys[i],
-      tempMax: ((data[solKeys[i]]["AT"]["mx"] - 32) * (5 / 9)).toFixed(1),
-      tempMin: ((data[solKeys[i]]["AT"]["mn"] - 32) * (5 / 9)).toFixed(1),
-      date: data[solKeys[i]]["First_UTC"].slice(0, -10),
-      season: data[solKeys[i]]["Season"],
-    }
+ 
+    const solDay = parseInt(solKeys[i]) + 1;
+    const date = data[solKeys[i]]["First_UTC"].slice(0, -10);
+    let tempMax = ((data[solKeys[i]]["AT"]["mx"] - 32) * (5 / 9)).toFixed(1);
+    let tempMin = ((data[solKeys[i]]["AT"]["mn"] - 32) * (5 / 9)).toFixed(1);
+    
+    $('.convert-temperature').on('click', () => {
+
+      // if it is showing celsius
+      if (tempMax === ((data[solKeys[i]]["AT"]["mx"] - 32) * (5 / 9)).toFixed(1) && tempMin === ((data[solKeys[i]]["AT"]["mn"] - 32) * (5 / 9)).toFixed(1)) {
+
+        
+        tempMax = ((data[solKeys[i]]["AT"]["mx"] * (9 / 5)) + 32).toFixed(1);
+        tempMin = ((data[solKeys[i]]["AT"]["mn"] * (9 / 5)) + 32).toFixed(1);
+
+        console.log(tempMax);
+        console.log(tempMin);
+        console.log('C');
+        
+        $('.temperature-max').text(`${tempMax} F°`);
+        $('.temperature-min').text(`${tempMin} F°`);
+
+        // if it is showing fahrenheit
+      } else {
+
+        tempMax = ((data[solKeys[i]]["AT"]["mx"] - 32) * (5 / 9)).toFixed(1);
+        tempMin = ((data[solKeys[i]]["AT"]["mn"] - 32) * (5 / 9)).toFixed(1);
+
+        console.log(tempMax);
+        console.log(tempMin);
+        console.log('F');
+
+        $('.temperature-max').text(`${tempMax} C°`);
+        $('.temperature-min').text(`${tempMin} C°`);
+
+      }
+
+    });
     
     const weatherEntry =
 	  `<div class="current-day-container-stats weekly-container-stats">
 	      <div class="sol-date-container">
 					<h2>
 						<i class="fas fa-sun" title="Sol Day"></i>
-						Sol ${dayStats.solDay}
+						Sol ${solDay}
 					</h2>
 					<h2>
 						<i class="fas fa-globe-americas" title="Current Day"></i> 
-						${dayStats.date}
+						${date}
 					</h2>
         </div>
         <div class="temp-container">
           <h3>
-            <i class="fas fa-temperature-high max-temp" title="Maximum Temperature"></i>
-            High: <span>${dayStats.tempMax} C°</span>
+            <i class="fas fa-temperature-high" title="Maximum Temperature"></i>
+            High: <span class="temperature-max">${tempMax} C°</span>
           </h3>
           <h3>
             <i class="fas fa-temperature-low min-temp" title="Minimum Temperature"></i>
-            Low: <span>${dayStats.tempMin} C°</span>
+            Low: <span class="temperature-min">${tempMin} C°</span>
           </h3>
         </div>
       </div>`
 
-    weeklyTemp.push(weatherEntry)
-
+      
     $('.weekly-weather-container').append(weatherEntry);
+    
+    weeklyTemp.push(weatherEntry)
     $('.current-day-container').append(weeklyTemp[6]);
     
   }
 }
 
-
-// app.convertTemperature = () => {
-//   $('.convert-temperature').on('click', () => {
-//     dayStats.tempMax = ((data[solKeys[i]]["AT"]["mx"] - 32) * (5 / 9)).toFixed(1),
-//       dayStats.tempMin = ((data[solKeys[i]]["AT"]["mn"] - 32) * (5 / 9)).toFixed(1),
-//       console.log(dayStats.tempMax);
-//     console.log(dayStats.tempMin);
-//   });
-// }
-
 // function temperatureConverter(valNum) {
 //   valNum = parseFloat(valNum);
 //   document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
 // } 
-
-
 
 app.init = () => {
   $.ajax({
